8e734951f877be302a337e38c92b793405e7ebb0
Ivan Shakhorski <ivanwanwas@gmail.com>
2021-02-06 18:37:12 +0300

    Implemented authorization

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "@/router/";
+import store from "@/store/";
+
+Vue.config.productionTip = false;
+Vue.use(Router);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
+
+if (localStorage.getItem("user")) {
+  store.commit("auth/userUpdated", JSON.parse(localStorage.getItem("user")));
+}
